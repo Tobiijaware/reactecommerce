ae09b6991352cbd32c20781e14ff92d0abe350ac
Tobiijaware <miketobicarter@gmail.com>
2021-06-17 09:33:32 -0700

    select and add to cart

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import './button.styles.scss';
 
-const Button = ({children, ...otherProps}) => (
-    <button className='custom-button' {...otherProps}>
+const Button = ({children, isGoogleSignin, inverted, ...otherProps}) => (
+    <button className={`${inverted ? 'inverted' : ''}
+    ${ isGoogleSignin ? 'google-sign-in' : '' }
+    custom-button`}
+     {...otherProps}
+    >
          {children}
     </button>
 );
--- src/components/cart-dropdown/cart.component.jsx
+++ src/components/cart-dropdown/cart.component.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './cart.styles.scss';
 import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
+import { connect } from 'react-redux';
 
-const Cart = () => (
+const Cart = ({ cartItems }) => (
     <div className='cart-dropdown'>
-        <div className='cart-items'></div>
+        <div className='cart-items'>
+            {cartItems.map(cartItem => (<CartItem key={cartItem.id} item={cartItem}/>))}
+        </div>
         <Button>GO TO CHECKOUT</Button>
     </div>
 );
 
-export default Cart;
+const mapStateToProps = ({cart: { cartItems}}) => ({
+    cartItems
+});
+
+export default connect(mapStateToProps)(Cart);
